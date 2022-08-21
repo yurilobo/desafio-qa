e8e96473a5c4b66490bc5f4304f810e50e192b61
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2022-08-21 16:59:27 -0300

    Ajustes

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,6 +23,6 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-Cypress.Commands.add('amazon', (email, password) => {
+Cypress.Commands.add('amazon', () => {
     cy.visit('https://www.amazon.com.br')
  })
