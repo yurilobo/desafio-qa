25a8875a7c98426114c3bb2a77f07c52f4edcfe3
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2022-08-21 18:36:24 -0300

    Ajustes segundo cenario

--- cypress/integration/CarrinhoDeCompras.spec.js
+++ cypress/integration/CarrinhoDeCompras.spec.js
@@ -1,5 +1,3 @@
-const { hasUncaughtExceptionCaptureCallback } = require("process")
-
 describe('Adicionando produto no Carrinho no site da Amazon', function() {
     it('Acessando o site da Amazon', function() {
         cy.amazon()
@@ -20,27 +18,29 @@ describe('Adicionando produto no Carrinho no site da Amazon', function() {
     
     it('Valide se o produto foi incluído no carrinho com sucesso', function() {
         cy.get('.a-color-base > .a-truncate > .a-truncate-cut').should('contains.text','Código limpo: Habilidades práticas do Agile Software')
-        cy.get('#sc-subtotal-amount-buybox > .a-size-medium').then((element)=>{
-            expect(element.text().replace('\n','').replace(' ','')).to.equal('R$71,88')
-        })
+        //cy.get('#activeCartViewForm > .sc-subtotal').should('contains.text','R$71,88')
+        
 
     })
    
 
   })
-  describe.skip('Adicionando 3 produtos no Carrinho no site da Amazon', function() {
+  describe('Adicionando 3 produtos no Carrinho no site da Amazon', function() {
     it('Acessando o site da Amazon', function() {
         cy.amazon()
        
     })
     it('Buscando e adicionando 3 produtos', function() {
         cy.get('#twotabsearchtextbox')
-        .type('echo 4{enter}', {force:true})
-        cy.get('[data-asin] > .sg-col-inner > .s-widget-container > .s-card-container > .a-spacing-base > .s-product-image-container > .rush-component > .a-link-normal > .a-section')
-        .first()
-        .click()
-        .get('#add-to-cart-button')
-        .click()
+            .type('codigo limpo {enter}', {force:true})
+            cy.get('[data-asin] > .sg-col-inner > .s-widget-container > .s-card-container > .a-spacing-base > .s-product-image-container > .rush-component > .a-link-normal > .a-section')
+            .first()
+            .click()
+            .get('#quantity').select('3').should('have.value', '3')    
+            .get('#add-to-cart-button')
+            .click()
+            .get('#sw-gtc > .a-button-inner > .a-button-text').click()
+        
     })
     
     it.skip('Validando o valor total no carrinho', function() {
