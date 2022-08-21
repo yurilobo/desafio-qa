39b921db656496eaa5a3ead9a6a1454b97daac9b
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2022-08-20 23:58:23 -0300

    Acessando o site da Amazon

---
+++ cypress/integration/adicionandoProduto.spec.js
@@ -0,0 +1,5 @@
+describe('Adicionando produto no Carrinho no site da Amazon', function() {
+    it('Acessando o site da Amazon', function() {
+        cy.visit('https://www.amazon.com.br')
+    })
+  })
